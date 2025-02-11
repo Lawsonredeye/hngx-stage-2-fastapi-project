8882f7f3fad9ea13aff7df59fbaaf3760a909fb4
Lawsonredeye <omoregbeeolawson@gmail.com>
2025-02-11 12:42:51 +0100

    fix: removed new route from main

--- main.py
+++ main.py
@@ -21,7 +21,3 @@ app.include_router(api_router, prefix=settings.API_PREFIX)
 async def health_check():
     """Checks if server is active."""
     return {"status": "active"}
-
-@app.get("/msg")
-async def hello():
-    return {"msg": "hello bot"}
